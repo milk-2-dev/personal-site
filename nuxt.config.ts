d76bac3b70049dd2d96083555e08c8cf222d03f0
milk <milk00000000000000@gmail.com>
2025-04-18 13:57:04 +0200

    veeßvalidate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,19 @@ export default defineNuxtConfig({
     "@nuxtjs/color-mode",
     "@vesp/nuxt-fontawesome",
     "@nuxt/image",
+    "@vee-validate/nuxt",
   ],
+  veeValidate: {
+    // disable or enable auto imports
+    autoImports: true,
+    // Use different names for components
+    componentNames: {
+      Form: "VeeForm",
+      Field: "VeeField",
+      FieldArray: "VeeFieldArray",
+      ErrorMessage: "VeeErrorMessage",
+    },
+  },
   fontawesome: {
     icons: {
       solid: [
