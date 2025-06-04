9f9f47d3a883a7139d820aa3c44626cc805ed61d
milk <milk00000000000000@gmail.com>
2025-06-04 18:39:00 +0200

    favicon

--- app.vue
+++ app.vue
@@ -22,7 +22,27 @@ useHead({
     { name: 'twitter:image', content: 'https://personal-site-ix15.onrender.com/og_image.jpg' },
     { name: 'twitter:image:alt', content: 'Roman Klimov | Senior frontend developer' },
   ],
+  link: [
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      href: '/apple-touch-icon.png'
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/favicon-32x32.png'
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: '/favicon-16x16.png'
+    },
+  ]
 })
+
 
 </script>
 
