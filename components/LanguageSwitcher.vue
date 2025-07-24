0f5ff21c655df551d3ba100d22cf65175adf3dc2
milk <milk00000000000000@gmail.com>
2025-07-24 14:00:29 +0200

    wip(ui): create select for switching between different languages

---
+++ components/LanguageSwitcher.vue
@@ -0,0 +1,49 @@
+<template>
+  <Select :defaultValue="currentLocale">
+    <SelectTrigger>
+      <SelectValue>
+        {{ currentLocale.toUpperCase() }}
+      </SelectValue>
+    </SelectTrigger>
+    <SelectContent>
+      <SelectGroup>
+        <SelectLabel>Languages:</SelectLabel>
+        <SelectItem
+          v-for="locale in locales"
+          :key="locale.code"
+          :value="locale.code"
+          :disabled="locale.code === currentLocale"
+          @select="switchLocale(locale.code)"
+        >
+          {{ locale.code.toUpperCase() }}
+        </SelectItem>
+      </SelectGroup>
+    </SelectContent>
+  </Select>
+</template>
+
+<script setup lang="ts">
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const { locales, locale, setLocale } = useI18n();
+
+const currentLocale = computed(() => {
+  return locale.value;
+});
+
+function switchLocale(code: any) {
+  if (code !== currentLocale.value) {
+    setLocale(code);
+  }
+}
+</script>
+
+<style scoped></style>
